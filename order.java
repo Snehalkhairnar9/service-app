2d7eadb90248fc715ebfbaeb7217570affe50ee5
L00163434 <91318384+Snehalkhairnar9@users.noreply.github.com>
2021-11-28 17:23:32 +0000

    Create order.java

---
+++ order.java
@@ -0,0 +1,20 @@
+nes (15 sloc)  328 Bytes
+   
+package com.javatechie.aws.cicd.example;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Order {
+
+    private int id;
+    private String name;
+    private int quantity;
+    private long price;
+}
